258bb4f69a10d5388810b9bf8abd93ede5384a3a
aqueefcs <aqueefalimansari007@gmail.com>
2023-02-14 02:21:20 +0530

    Msg for Jenkins

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ app.get("/",(req,res)=>{
     try{
 
         res.send("Jenkins Testing by Aqueef...");
+        console.log("Running in Jenkins")
 
     }catch(err){
         console.log(err.message);
     }
 })
 
-app.listen(3000,(req,res)=>{
-    console.log("App is running on port:3000");
-})
+setTimeout((req,res)=>{
+    app.listen(3000,(req,res)=>{
+        console.log("App is running on port:3000");
+    })
+},5000)
+
