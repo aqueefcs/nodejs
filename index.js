c101afc059dd7ed9b6775b5fc90d02254cd41caf
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2023-02-17 17:14:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get("/",(req,res)=>{
-    try{
+// app.get("/",(req,res)=>{
+//     try{
 
-        res.send("Jenkins Testing by Aqueef...");
-        console.log("Running in Jenkins")
+//         res.send("Jenkins Testing by Aqueef...");
+//         console.log("Running in Jenkins")
 
-    }catch(err){
-        console.log(err.message);
-    }
-})
+//     }catch(err){
+//         console.log(err.message);
+//     }
+// })
 
-app.listen(3000,(req,res)=>{
-    console.log("App is running on port:3000");
-})
+// app.listen(3000,(req,res)=>{
+//     console.log("App is running on port:3000");
+// })
+
+const process = require('process');
+  
+// Printing process.argv property value
+const input = process.argv[2]
+
+console.log('Input Recived: ',input)
 
