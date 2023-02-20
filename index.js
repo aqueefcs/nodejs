106bdf9c7f0b52682fc9d56acc390eb9bcee3268
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2023-02-20 17:55:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,43 +23,43 @@
 
 // console.log('Input Recived: ',input.name)
 
-// const process = require('process');
+const process = require('process');
   
-// // Printing process.argv property value
-// const input = process.argv[2]
+// Printing process.argv property value
+const input = process.argv[2]
 
-// console.log('Input Recived: ',input)
+console.log('Input Recived: ',input)
 
 
-const express = require('express')
-const app = express();
+// const express = require('express')
+// const app = express();
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/',(req,res)=>{
-    try{
-        res.json({message:"This is the home page"});
-    }catch(err){
-        console.log(err.message)
-    }
-})
+// app.get('/',(req,res)=>{
+//     try{
+//         res.json({message:"This is the home page"});
+//     }catch(err){
+//         console.log(err.message)
+//     }
+// })
 
-app.post("/api/input",(req,res)=>{
-    try{
-        const input = req.body;
-        if(input){
+// app.post("/api/input",(req,res)=>{
+//     try{
+//         const input = req.body;
+//         if(input){
 
-            res.json({msg:'Input recieved successfully',data:input});
-        }else {
-            res.json({msg:"Input not recieved"})
-        }
+//             res.json({msg:'Input recieved successfully',data:input});
+//         }else {
+//             res.json({msg:"Input not recieved"})
+//         }
 
-    }catch(err){
-        console.log(err.message);
-    }
-})
+//     }catch(err){
+//         console.log(err.message);
+//     }
+// })
 
-app.listen(3000,()=>{
-    console.log("App is running...")
-})
+// app.listen(3000,()=>{
+//     console.log("App is running...")
+// })
 
