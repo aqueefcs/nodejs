d252bc1f4a4fa9ccd31bc93d69c13496da8d90bc
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2023-02-23 16:15:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 require('dotenv/config')
-const jsonPayload = process.env.payload
+const jsonPayload = JSON.parse(process.env.payload)
 console.log("name: ", jsonPayload);
 console.log("name: ", typeof(jsonPayload));
 
