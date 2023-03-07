d049a5108589ce28e9f93b294cd58508c4e4b07f
aqueefcs <53555185+aqueefcs@users.noreply.github.com>
2023-03-07 15:16:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const process = require('process');
 require('dotenv/config')
 console.log("pay: ",process.argv[2]);
+const jsonPayload = JSON.parse(process.argv[2])
+console.log("jp: ",jsonPayload)
 // const jsonPayload = JSON.parse(process.env.payload)
 // console.log("name: ", jsonPayload);
 // console.log("name: ", typeof(jsonPayload));
